00173c6ccbee1d894393dca8aa214804654ef14a
Dmitri Rubinstein <dmitri.rubinstein@dfki.de>
2023-10-12 14:42:09 +0200

    Changes for UnityTest

--- ubuntuWithNvidiaDriver/build.sh
+++ ubuntuWithNvidiaDriver/build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-IMAGE_NAME=ubuntu_with_nvidia_driver
-NVIDIA_DRIVER=NVIDIA-Linux-x86_64-337.12.run  # path to nvidia driver
+THIS_DIR=$(dirname "$0")
+cd "$THIS_DIR"
 
-cp ${NVIDIA_DRIVER} NVIDIA-DRIVER.run
-sudo docker build -t ${IMAGE_NAME} .
+IMAGE_NAME=ubuntu_with_nvidia_driver
+NVIDIA_DRIVER=NVIDIA-Linux-x86_64-355.11.run
+
+#chmod +x "${NVIDIA_DRIVER}"
+#cp "${NVIDIA_DRIVER}" NVIDIA-DRIVER.run
+
+docker build --build-arg NVIDIA_DRIVER="$NVIDIA_DRIVER" -t "$IMAGE_NAME" .
